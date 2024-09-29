06b6e01047e5d12c1673c131bc46a449f9331bbc
Guilherme <guiduartesbs@hotmail.com>
2024-09-29 15:30:08 -0300

    minor

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -723,9 +723,9 @@ export default function DemoPage() {
                         boxShadow: "0 1px 1px #0c192714, 0 1px 3px #0c192724",
                       }}
                     >
-                      Restart demo
+                      Reiniciar
                     </button>
-                    <Link
+                    {/* <Link
                       href="https://github.com/Tameyer41/liftoff"
                       target="_blank"
                       className="group rounded-full pl-[8px] min-w-[180px] pr-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#1E2B3A] text-white hover:[linear-gradient(0deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)), #0D2247] no-underline flex gap-x-2  active:scale-95 scale-100 duration-75"
@@ -757,7 +757,7 @@ export default function DemoPage() {
                         </svg>
                       </span>
                       Star on Github
-                    </Link>
+                    </Link> */}
                   </div>
                 </div>
               )}
